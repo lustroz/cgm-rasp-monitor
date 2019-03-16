09a00ff5475ae8817f74c0c7c7c024912a394405
do <dolucy.dev@gmail.com>
2019-03-16 09:06:28 +0900

    handle crash

--- cgm.py
+++ cgm.py
@@ -17,23 +17,30 @@ logger.addHandler(fileHandler)
 
 database.createTable()
 
-while True:
-    # print('\n***** retrieve server data *****\n')
-    nightscout.getEntries()
+def main():
+    while True:
+        # print('\n***** retrieve server data *****\n')
+        nightscout.getEntries()
 
-    # print('\n***** fetch entries from db *****\n')
+        # print('\n***** fetch entries from db *****\n')
 
-    rows = database.fetchEntries()
-    # for row in rows:
-    #     print(row)
+        rows = database.fetchEntries()
+        # for row in rows:
+        #     print(row)
 
-    if len(rows) > 1:
-        delta = rows[0][3] - rows[1][3]
-    else:
-        delta = 0
+        if len(rows) > 1:
+            delta = rows[0][3] - rows[1][3]
+        else:
+            delta = 0
 
-    if len(rows) > 0:
-        latest = rows[0]
-        oled.draw(latest[1], latest[2], latest[3], latest[4], delta)
+        if len(rows) > 0:
+            latest = rows[0]
+            oled.draw(latest[1], latest[2], latest[3], latest[4], delta)
 
-    time.sleep(60)
+        time.sleep(60)
+
+
+try:
+    main()
+except Exception as e:
+    logger.exception('main crashed. Error: %s', e)
