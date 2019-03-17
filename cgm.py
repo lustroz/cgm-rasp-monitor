5d83d6075df280f5ffefd7977eaf1c5ed595b7b1
do <dolucy.dev@gmail.com>
2019-03-17 17:23:42 +0900

    temp

--- cgm.py
+++ cgm.py
@@ -4,7 +4,7 @@ import nightscout
 import database
 import time
 import oled
-import bluetooth
+import btutil
 import logging
 import logging.handlers
 import asyncio
@@ -24,27 +24,36 @@ streamHandler = logging.StreamHandler()
 streamHandler.setFormatter(formatter)
 logger.addHandler(streamHandler)
 
-database.createTable()
-
 _state = state.State()
 _data = data.Data()
 
 class AsyncTask:
-    def fetchData(self):
-        _data.fetchData(_state)
-        threading.Timer(30, self.fetchData).start()
 
     def process(self):
-        _state.process()
-        threading.Timer(_state.interval, self.process).start()
+        db = database.Database()
+        db.createTable()
+
+        while True:
+            _data.fetchData(_state, db)
+            _state.process(db)
+
+            time.sleep(_state.interval)
        
 try:
     task = AsyncTask()
-    task.fetchData()
-    task.process()
-    
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(asyncio.wait([bluetooth.listen()]))
+
+    threads = []
+    t = threading.Thread(target=btutil.start)
+    threads.append(t)
+    t = threading.Thread(target=task.process)
+    threads.append(t)
+
+    for t in threads:
+        t.start()
+
+    for t in threads:
+        t.join()
+
 
 except Exception as e:
     logger.exception('main crashed. Error: %s', e)
