24e0f1092b526983058614acd3d5ad9cf1399f40
do <lustro0@gmail.com>
2019-05-31 07:39:55 +0900

    add dependency modules

--- install3.sh
+++ install3.sh
@@ -1,4 +1,10 @@
-sudo apt-get install python3-dev python3-pip libfreetype6-dev libjpeg-dev
+sudo apt install bluez libbluetooth-dev pi-bluetooth
+sudo apt install -y python3-dev python3-pip libfreetype6-dev libjpeg-dev
+sudo sed -i "s/bluetoothd/bluetoothd -C/g" /lib/systemd/system/bluetooth.service
+sudo systemctl daemon-reload 
+sudo systemctl restart bluetooth
+sudo systemctl enable bluetooth
 sudo -H pip3 install --upgrade luma.oled
-python3 -m pip install pybluez
+sudo pip3 install simplejson pybluez pybleno
+sudo apt install fonts-lato
 
