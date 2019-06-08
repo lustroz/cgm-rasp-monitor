ffca4292e4d1e13615f4937579eeb74105aab528
do <lustro0@gmail.com>
2019-06-08 21:06:15 +0900

    .

--- install3.sh
+++ install3.sh
@@ -1,10 +1,10 @@
-sudo apt install bluez libbluetooth-dev pi-bluetooth
-sudo apt install -y python3-dev python3-pip libfreetype6-dev libjpeg-dev
+sudo apt install -y python3-dev libffi-dev libssl-dev python3-pip libfreetype6-dev libjpeg-dev bluez libbluetooth-dev pi-bluetooth
+sudo -H pip3 install --upgrade luma.oled
+sudo pip3 install simplejson pybluez pybleno
+sudo pip3 install python-telegram-bot --upgrade
+sudo apt install fonts-lato
 sudo sed -i "s/bluetoothd/bluetoothd -C/g" /lib/systemd/system/bluetooth.service
 sudo systemctl daemon-reload 
 sudo systemctl restart bluetooth
 sudo systemctl enable bluetooth
-sudo -H pip3 install --upgrade luma.oled
-sudo pip3 install simplejson pybluez pybleno
-sudo apt install fonts-lato
 
