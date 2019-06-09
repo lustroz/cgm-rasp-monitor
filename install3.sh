b23761a53c037de6cecae2710d578f9314e591a9
do <lustro0@gmail.com>
2019-06-09 16:02:48 +0900

    modified install3.sh for pip timeout

--- install3.sh
+++ install3.sh
@@ -1,7 +1,7 @@
 sudo apt install -y python3-dev libffi-dev libssl-dev python3-pip libfreetype6-dev libjpeg-dev bluez libbluetooth-dev pi-bluetooth
 sudo -H pip3 install --upgrade luma.oled
 sudo pip3 install simplejson pybluez pybleno
-sudo pip3 install python-telegram-bot --upgrade
+sudo pip3 install python-telegram-bot --upgrade --default-timeout=100
 sudo apt install fonts-lato
 sudo sed -i "s/bluetoothd/bluetoothd -C/g" /lib/systemd/system/bluetooth.service
 sudo systemctl daemon-reload 
