422f7362a3a246f64c6b115b5a08e8c3444952bc
do <lustro0@gmail.com>
2019-06-13 23:27:03 +0900

    .

--- tgutil/__init__.py
+++ tgutil/__init__.py
@@ -4,33 +4,36 @@ import setting
 
 logger = logging.getLogger('cgm')
 
+_chatId = 0
+
 def sendLatestEntry(db):
     config = setting.getCurrent()
     
-    try:
-        bot = telegram.Bot(token = config['tg_bot_token'])
-        updates = bot.getUpdates()
-        if len(updates) == 0:
-            return
+    bot = telegram.Bot(token = config['tg_bot_token'])
+    updates = bot.getUpdates()
+    if len(updates) == 0:
+        return
 
-        r = db.getDisplayValues()
-        if r['val'] == 0:
-            return
+    r = db.getDisplayValues()
+    if r['val'] == 0:
+        return
 
-        latest = updates[-1]
-        # logger.info(latest)
-        
-        if latest.channel_post != None:
-            chatId = latest.channel_post.chat.id
-        elif latest.message != None:
-            chatId = latest.message.chat.id
-        else:
-            return
+    latest = updates[-1]
+    # logger.info(latest)
 
-        m = int(r['elapsed'] / 60)
+    global _chatId
+    
+    if latest.channel_post != None:
+        chatId = latest.channel_post.chat.id
+        _chatId = chatId
+    elif latest.message != None:
+        chatId = latest.message.chat.id
+        _chatId = chatId
+    else:
+        chatId = _chatId
 
-        msg = '혈당: {2}\n\n지난 시간: {0} 분\n변동량: {1}\n방향성: {3}'.format(m, r['delta'], r['val'], r['direction'])
-        bot.sendMessage(chat_id = chatId, text = msg)
-    except:
-        pass
+    m = int(r['elapsed'] / 60)
+
+    msg = '혈당: {2}\n\n지난 시간: {0} 분\n변동량: {1}\n방향성: {3}'.format(m, r['delta'], r['val'], r['direction'])
+    bot.sendMessage(chat_id = chatId, text = msg)
     
